408eaa704bf944687b776fc96f8b7773e7192de5
oelkapmis <oguzhan.elkapmis@gmail.com>
2022-05-28 17:33:24 +0300

    Add register screen and setup Firebase

---
+++ app/src/main/java/com/example/yazyaz/Common/Common.java
@@ -0,0 +1,8 @@
+package com.example.yazyaz.Common;
+
+import com.example.yazyaz.Model.UserModel;
+
+public class Common {
+    public static UserModel currentUser = new UserModel();
+    public static final String USER_REFERENCES = "People";
+}
